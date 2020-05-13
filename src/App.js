f16e8b9bbdbf4f59887d8bb19b2d66e6be785c94
Yvonne <tangy222@gmail.com>
2020-05-13 23:35:21 +0100

    can move all cells to left, right, down or up

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Header from "./component/Header";
 
 function App() {
   const [grid, setGrid] = useState([
-    [32, 0, 0, 0],
-    [8, 0, 0, 0],
-    [16, 0, 0, 0],
-    [128, 0, 0, 0],
+    [0, 0, 4, 0],
+    [256, 0, 0, 64],
+    [0, 8, 0, 0],
+    [8, 2, 0, 32],
   ]);
   const [newBlock, setNewBlock] = useState(0);
   const [score, setScore] = useState(0);
@@ -27,7 +27,6 @@ function App() {
     setGrid(gridCopy);
     setScore(newBlock);
   }, [resetToggle]);
-
 
   // clicking RESET
   const init = () => {
@@ -58,14 +57,111 @@ function App() {
     return blockIds[randIndex];
   };
 
-  // shift LEFT eventlistener
-  
+  const shiftUp = () => {
+    let numsOnly;
+    let gridCopy = [...grid];
+    for (let col = 0; col < 4; col++) {
+      let column = [gridCopy[0][col], gridCopy[1][col], gridCopy[2][col], gridCopy[3][col]];
+      numsOnly = column.filter((num) => num > 0);
+      gridCopy[0][col] = numsOnly[0] !== undefined ? numsOnly[0] : 0;
+      gridCopy[1][col] = numsOnly[1] !== undefined ? numsOnly[1] : 0;
+      gridCopy[2][col] = numsOnly[2] !== undefined ? numsOnly[2] : 0;
+      gridCopy[3][col] = numsOnly[3] !== undefined ? numsOnly[3] : 0;
+    }
+    setGrid(gridCopy);
+  };
 
+  const shiftDown = () => {
+    let numsOnly;
+    let gridCopy = [...grid];
+    for (let col = 0; col < 4; col++) {
+      let column = [
+        gridCopy[0][col],
+        gridCopy[1][col],
+        gridCopy[2][col],
+        gridCopy[3][col],
+      ];
+      numsOnly = column.filter((num) => num > 0);
+      numsOnly = numsOnly.reverse();
+      gridCopy[3][col] = numsOnly[0] !== undefined ? numsOnly[0] : 0;
+      gridCopy[2][col] = numsOnly[1] !== undefined ? numsOnly[1] : 0;
+      gridCopy[1][col] = numsOnly[2] !== undefined ? numsOnly[2] : 0;
+      gridCopy[0][col] = numsOnly[3] !== undefined ? numsOnly[3] : 0;
+    }
+    setGrid(gridCopy);
+  };
+
+  const shiftRight = () => {
+    let numsOnly;
+    let gridCopy = [...grid];
+    for (let row = 0; row < 4; row++) {
+      let column = [
+        gridCopy[row][0],
+        gridCopy[row][1],
+        gridCopy[row][2],
+        gridCopy[row][3],
+      ];
+      numsOnly = column.filter((num) => num > 0);
+      numsOnly = numsOnly.reverse();
+      gridCopy[row][3] = numsOnly[0] !== undefined ? numsOnly[0] : 0;
+      gridCopy[row][2] = numsOnly[1] !== undefined ? numsOnly[1] : 0;
+      gridCopy[row][1] = numsOnly[2] !== undefined ? numsOnly[2] : 0;
+      gridCopy[row][0] = numsOnly[3] !== undefined ? numsOnly[3] : 0;
+    }
+    setGrid(gridCopy);
+  };
+
+  const shiftLeft = () => {
+    let numsOnly;
+    let gridCopy = [...grid];
+    for (let row = 0; row < 4; row++) {
+      let column = [
+        gridCopy[row][0],
+        gridCopy[row][1],
+        gridCopy[row][2],
+        gridCopy[row][3],
+      ];
+      numsOnly = column.filter((num) => num > 0);
+      numsOnly = numsOnly;
+      gridCopy[row][0] = numsOnly[0] !== undefined ? numsOnly[0] : 0;
+      gridCopy[row][1] = numsOnly[1] !== undefined ? numsOnly[1] : 0;
+      gridCopy[row][2] = numsOnly[2] !== undefined ? numsOnly[2] : 0;
+      gridCopy[row][3] = numsOnly[3] !== undefined ? numsOnly[3] : 0;
+    }
+    setGrid(gridCopy);
+  };
+
+  const swipe = (e) => {
+    if (e.key === "ArrowUp") {
+      shiftUp();
+    } else if (e.key === "ArrowDown") {
+      shiftDown();
+    } else if (e.key === "ArrowLeft") {
+      shiftLeft();
+    } else if (e.key === "ArrowRight") {
+      shiftRight();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", swipe);
+
+    return () => {
+      window.removeEventListener("keydown", swipe);
+    };
+  });
+
+  // eventlisteners for up, down, left and right arrow keys
 
   return (
     <div className="app">
       {console.log(grid)}
-      <Header score={score} bestScore={bestScore} init={init} playing={playing}/>
+      <Header
+        score={score}
+        bestScore={bestScore}
+        init={init}
+        playing={playing}
+      />
 
       <main className="grid">
         {grid
