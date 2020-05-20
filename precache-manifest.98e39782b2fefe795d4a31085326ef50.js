cfff53ab8ffb8c28dcf622bfc268a2fa1a072e83
Yvonne <tangy222@gmail.com>
2020-05-20 18:55:31 +0100

    Updates

---
+++ precache-manifest.98e39782b2fefe795d4a31085326ef50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "948170bc922e698983584d920c6c0c0b",
+    "url": "/2048-game/index.html"
+  },
+  {
+    "revision": "e9b545893c2b5134bcfc",
+    "url": "/2048-game/static/css/main.cdb131db.chunk.css"
+  },
+  {
+    "revision": "578d6f223f232323b72c",
+    "url": "/2048-game/static/js/2.aa71feb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2048-game/static/js/2.aa71feb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9b545893c2b5134bcfc",
+    "url": "/2048-game/static/js/main.078249ce.chunk.js"
+  },
+  {
+    "revision": "a227264629f6d9f6530f",
+    "url": "/2048-game/static/js/runtime-main.c20a4da1.js"
+  }
+]);
