37a8e271433001cd8f566a2af932f16e20df865f
Yvonne <tangy222@gmail.com>
2020-08-13 22:02:09 +0100

    ammend text and scroll

--- src/component/Instructions.jsx
+++ src/component/Instructions.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { MobileView, BrowserView } from 'react-device-detect';
 
 export default function Instructions() {
   return (
     <div className='instructions'>
-          <strong>How to play: </strong>Press the ↑, ↓, →, ← arrow keys
-      to move the tiles. When 2 same tiles touch they merge into one. Reach 2048
-      to win.
+      <BrowserView>
+        <strong>How to play: </strong> Press the ↑, ↓, →, ← arrow keys to move
+        the tiles. When 2 same tiles touch they merge into one. Reach 2048 to
+        win.
+      </BrowserView>
+      <MobileView>
+        <strong>How to play: </strong> Swipe up, right, down and left to move
+        the tiles. When 2 same tiles touch they merge into one. Reach 2048 to
+        win.
+      </MobileView>
     </div>
   );
 }
